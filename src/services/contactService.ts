35c842fed5ae12a2f22f2299034f4d8bff830fa9
andref03 <andref.lopes@outlook.com>
2025-10-12 12:27:57 -0300

    implementando service com lógica do problema e interagindo com BD via Prisma

---
+++ src/services/contactService.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import type { Contact } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getContacts = async (): Promise<Contact[]> => {
+	return prisma.contact.findMany();
+};
+
+export const createContact = async (data: {
+	name: string;
+	email: string;
+	phone?: string | null;
+}) => {
+	return prisma.contact.create({ data });
+};
+
+export const updateContact = async (
+	id: number,
+	data: { name?: string; email?: string; phone?: string | null }
+) => {
+	return prisma.contact.update({ where: { id }, data });
+};
+
+export const deleteContact = async (id: number) => {
+	return prisma.contact.delete({ where: { id } });
+};
