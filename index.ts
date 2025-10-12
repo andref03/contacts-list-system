e45b14b02b7908e7ad3080b4b72a97507763ba4d
andref03 <andref.lopes@outlook.com>
2025-10-12 12:26:00 -0300

    inicializando servidor com estrutura básica

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import contactRoutes from "./src/routes/contactRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/contacts", contactRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
